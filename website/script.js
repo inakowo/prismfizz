fce238b0a4e5881c34008d42ba9e60a00821984a
inakowo <194451890+inakowo@users.noreply.github.com>
2025-04-30 12:05:00 +0700

    Delete website directory

--- website/script.js
+++
@@ -1,8 +0,0 @@
-elementImg= document.getElementById("idImage");
-listImage= ["menu.jpg", ".png", ".png"];
-i=0;
-function gantiGambar(){
-    elementImg.src= listImage[i];
-    i= (i + 1) % listImage.length;
-}
-setInterval(gantiGambar, 5000)
